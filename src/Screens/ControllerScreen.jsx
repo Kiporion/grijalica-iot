c09f5900e715d82cf184c871e0028a814a8a82b1
ilhanneziric <ilhan.neziric@edu.fit.ba>
2022-09-23 23:12:25 +0200

    Update requests

--- src/Screens/ControllerScreen.jsx
+++ src/Screens/ControllerScreen.jsx
@@ -1,26 +1,42 @@
-import { onValue, ref } from "firebase/database";
+import { onValue, ref, set } from "firebase/database";
 import { useEffect, useState } from "react"
 import { db } from "../firebase"
 
 
 const ControllerScreen = () => {
 
-  const [jacina, setJacina] = useState(100);
+  const [jacina, setJacina] = useState(0);
 
   useEffect(() => {
    onValue(ref(db), snapshot => {
     const data = snapshot.val();
     setJacina(data.jacina);
    });
-  }, [])
-  
+  }, []);
+
+  const pojacaj = () => {
+    set(ref(db, '/jacina'), jacina - 0.1);
+  }
+
+  const smanji = () => {
+    set(ref(db, '/jacina'), jacina + 0.1);
+  }
+
+  const on = () => {
+    set(ref(db, '/jacina'), 1);
+  }
+
+  const off = () => {
+    set(ref(db, '/jacina'), 0);
+  }
+
   return (
     <>
       <h1>{jacina}</h1>
-      <button>ON</button>
-      <button>OFF</button>
-      <button>POJACAJ</button>
-      <button>SMANJI</button>
+      <button onClick={on}>ON</button>
+      <button onClick={off}>OFF</button>
+      <button onClick={pojacaj}>POJACAJ</button>
+      <button onClick={smanji}>SMANJI</button>
     </>
   )
 }
